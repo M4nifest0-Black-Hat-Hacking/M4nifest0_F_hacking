313fce4e7262369a36aa646cd0018975596ad612
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2021-06-24 14:07:30 +0000

    Add files via upload

---
+++ M4nifest0_F_hacking.rb
@@ -0,0 +1,23 @@
+=begin
+
+M4nifest0 Black Hat Hacking Team™💪
+𝕿𝖍𝖎𝖘 𝕴𝖘 𝕿𝖍𝖊 𝓜4𝓷𝓲𝓯𝓮𝓼𝓽0 𝕿𝖊𝖆𝖒™
+Author hack4lx 👊 
+💪 Site : https://m4nifest0.com 👊
+💪 Site : https://m4nifest0.group 👊
+💪 Site : https://m4nifest0.shop 👊
+💪 Channel Telegram : https://t.me/M4nifest0 👊
+2021 💪
+Version 1.0.0 💪
+
+
+Abut:
+=============
+
+🔞The M4nifest0 (M4) team
+☠️ is a security team from the Black Hat hacker group
+🏴‍☠️that has set up public sites👀
+destroyed government sites🦠
+and disclosed information around the world to enforce people's rights.💀💪
+https://t.me/M4nifest0
+=end
